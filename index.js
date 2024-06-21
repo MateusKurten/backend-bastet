c11a8c41e3eec3f65a3a96ad7f04ebc44148cad6
Mateus Kürten <eng.mateuskurten@gmail.com>
2024-06-20 21:11:13 -0300

    disable cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { auth } = require('./middleware/auth');
 const port = 3333;
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true
 }));
 app.use(express.json());
