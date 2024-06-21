017c62b58fa6bdfb6064006a073a1d5056a5a08f
Mateus Kürten <eng.mateuskurten@gmail.com>
2024-06-21 20:23:47 -0300

    remove coluna hora_cancelamento

--- controller/alunoController.js
+++ controller/alunoController.js
@@ -95,15 +95,10 @@ const cancelarInscricao = async (req, res) => {
         }
 
         const dataCancelamento = new Date();
-        const horas = dataCancelamento.getHours();
-        const minutos = dataCancelamento.getMinutes();
-        const segundos = dataCancelamento.getSeconds();
-        const horaAtual = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
 
         await sql`UPDATE aluno_curso_bastet SET 
             cancelado = 1,
-            data_cancelamento = ${dataCancelamento.toISOString()},
-            hora_cancelamento= ${horaAtual}
+            data_cancelamento = ${dataCancelamento.toISOString()}
             WHERE id_aluno = ${req.aluno.id} 
             AND id_curso = ${curso.rows[0].id}`;
 
